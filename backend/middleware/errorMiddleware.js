198462b7dc71760ffc706dc1c20b3b6510ad9d3d
Cualabeer <carabella187@icloud.com>
2025-08-29 18:52:05 +0100

    Update errorMiddleware.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,3 +1,4 @@
+// ./middleware/errorMiddleware.js
 export const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
@@ -6,7 +7,8 @@ export const notFound = (req, res, next) => {
 
 export const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode).json({
+  res.status(statusCode);
+  res.json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
