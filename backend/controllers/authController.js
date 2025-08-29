10aadf7f8d9f6217e52c6a559f50890bedf1a3b7
Cualabeer <carabella187@icloud.com>
2025-08-29 12:34:45 +0100

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,12 +1,14 @@
 import User from "../models/User.js";
 import generateToken from "../utils/generateToken.js";
 
+// Register
 export const registerUser = async (req, res) => {
   const { name, email, password, role } = req.body;
-  const userExists = await User.findOne({ email });
-  if (userExists) return res.status(400).json({ message: "User already exists" });
+  const existingUser = await User.findOne({ email });
+  if (existingUser) return res.status(400).json({ message: "User already exists" });
 
   const user = await User.create({ name, email, password, role });
+
   res.status(201).json({
     _id: user._id,
     name: user.name,
@@ -16,6 +18,7 @@ export const registerUser = async (req, res) => {
   });
 };
 
+// Login
 export const authUser = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
