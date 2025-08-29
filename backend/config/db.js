eabf3b06ac1747b829ef4c958cab0325c01e9c0a
Cualabeer <carabella187@icloud.com>
2025-08-29 12:22:54 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(err);
-    process.exit(1);
+    console.error(`❌ MongoDB connection error: ${err.message}`);
+    process.exit(1); // exit the app if DB connection fails
   }
 };
 
